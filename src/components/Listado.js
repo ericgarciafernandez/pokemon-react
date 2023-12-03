52b14f7b01da55a7e399be6b49bec60bc276928b
ericgarciafernandez <ericgarciafernandez>
2023-12-03 02:46:17 +0100

    set Loading and error component

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { Table, TableBody, TableCell, TableRow, TableContainer, TableHead, TablePagination, Button, Container } from "@mui/material";
 import '@fontsource/roboto/500.css';
-import Detalles from "./Detalles";
 import { v4 as uuidv4 } from 'uuid';
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Detalles from "./Detalles";
+import Loading from "../screens/Loading";
+import Error from "../screens/Error";
+
 
 const Listado = () => {
     const [pokedex, setPokedex] = useState([]); // Listado de pokemons
@@ -54,8 +57,8 @@ const Listado = () => {
                                         <Button onClick={handleDetails.bind(null, pokemon.name)} variant="contained">Entrar</Button>
                                     </TableCell>
                                 </TableRow>
-                            )) : 'Loading...'}
-                            {error ? <p>{error}</p> : ''}
+                            )) : <Loading />}
+                            {error ? <Error errorMsg={error} /> : ''}
                         </TableBody>
                     </Table>
                 </TableContainer>
